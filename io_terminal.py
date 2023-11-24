20ce682bfbc575e3ae2e4ee7a2b2f308a15adab4
ps74558release <ps74558release@gmail.com>
2023-11-24 15:27:58 +0000

    Adcionada afunção 'def pergunta_id()'

--- io_terminal.py
+++ io_terminal.py
@@ -1,10 +1,48 @@
 from tabulate import tabulate
 
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_terminal*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_terminal.py existente, deve apagar
-# todos os outros ficheiros io_terminal-*.py, e inclusive estes comentários
+def pergunta_id(questao, lista, mostra_lista=False):
+    """TODO: documentação
 
-# ...
+    :param questao:
+    :param lista:
+    :param mostra_lista:
+    :return:
+    """
+
+    if mostra_lista:
+        imprime_lista(cabecalho="", lista=lista)
+
+    while True:
+        id = int(input(questao))
+        if 0 <= id < len(lista):
+            return id
+        else:
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
+
+
+def pause():
+    """Faz uma pausa no programa e espera que o utilizador pressione ENTER"""
+
+    input("Pressione ENTER para continuar...")
+
+
+
+def pergunta_id(questao, lista, mostra_lista=False):
+    """TODO: documentação
+
+    :param questao:
+    :param lista:
+    :param mostra_lista:
+    :return:
+    """
+
+    if mostra_lista:
+        imprime_lista(cabecalho="", lista=lista)
+
+    while True:
+        id = int(input(questao))
+        if 0 <= id < len(lista):
+            return id
+        else:
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
