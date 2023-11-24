bccf8d959dcb7cc560db54671b7e5b81eb32fbd8
ps74558develop <ps74558develop@gmail.com>
2023-11-24 14:52:07 +0000

    Adicionada a função 'def pergunta_id()'

--- io_terminal.py
+++ io_terminal.py
@@ -1,6 +1,26 @@
 from tabulate import tabulate
 
 
+def pergunta_id(questao, lista, mostra_lista=False):
+    """TODO: documentação
+
+    :param questao:
+    :param lista:
+    :param mostra_lista:
+    :return:
+    """
+
+    if mostra_lista:
+        imprime_lista(cabecalho="", lista=lista)
+
+    while True:
+        id = int(input(questao))
+        if 0 <= id < len(lista):
+            return id
+        else:
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
+            
+
 # TODO: Copie para aqui o código de cada uma das funções nos
 # ficheiros com o nome io_terminal*.py e faça um commit de cada vez
 # Quando este ficheiro estiver completo com todas as suas funções,
