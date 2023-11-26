4945e957442531810653db979998a6d287007490
Releaser <psa74558release@gmail.com>
2023-11-26 15:32:07 +0000

    Repsoitório restruturado, ficheiros *.py movidos para /src

---
+++ src/faturas.py
@@ -0,0 +1,31 @@
+from datetime import date
+
+from io_terminal import imprime_lista, pergunta_id
+
+nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
+
+
+def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
+    """Pede ao utilizador para introduzir os dados de uma nova fatura
+
+    :return: dicionario com uma fatura, na forma
+        {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
+    """
+
+    id_cliente = pergunta_id(questao="Qual o id do cliente?", lista=lista_de_clientes, mostra_lista=True)
+    id_veiculo = pergunta_id(questao="Qual o id do veiculo?", lista=lista_de_veiculos, mostra_lista=True)
+
+    # TODO: Pedir o resto dos dados da fatura, e não esquecer de os guardar no dicionario
+    # ...
+
+    fatura = {"cliente": id_cliente,
+              "veiculo": id_veiculo,
+              "data": date.today()}
+
+    return fatura
+
+
+def imprime_lista_de_faturas(lista_de_faturas):
+    """Imprime a lista de faturas"""
+
+    imprime_lista_de_faturas(cabecalho="Lista de Faturas", lista=lista_de_faturas)
